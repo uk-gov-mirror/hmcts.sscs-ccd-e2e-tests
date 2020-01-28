7b586798968dbb624410ce30dc32475b7f7358c1
lee365 <leephilip365@yahoo.co.uk>
2020-01-28 10:32:43 +0000

    Signout and get to url

--- e2e/service.conf.js
+++ e2e/service.conf.js
@@ -1,6 +1,6 @@
 module.exports = {
   CcdGatewayUrl: process.env.TEST_E2E_URL_GATEWAY || 'https://gateway-ccd.aat.platform.hmcts.net',
-  CcdWebUrl: process.env.TEST_E2E_URL_WEB || 'https://www-ccd.aat.platform.hmcts.net/list/case?jurisdiction=sscs',
+  CcdWebUrl: process.env.TEST_E2E_URL_WEB || 'https://www-ccd.aat.platform.hmcts.net',
   UseHeadlessBrowser: process.env.TEST_E2E_HEADLESS !== 'false',
  
   TestCaseOfficerUserName: process.env.TEST_CASEOFFICER_USERNAME || '',
