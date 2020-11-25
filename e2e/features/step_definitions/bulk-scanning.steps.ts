9a1231c749e776ddc0381d5335bcdf6799373548
rentaluser <rentaluser@rentalusers-mbp.home>
2020-11-25 12:12:03 +0000

    Fix ccd-tests

--- e2e/features/step_definitions/bulk-scanning.steps.ts
+++ e2e/features/step_definitions/bulk-scanning.steps.ts
@@ -70,7 +70,7 @@ Given(/^I have a (.+) bulk-scanned document with (?:all fields)$/, {timeout: 600
 
     expect(await anyCcdPage.pageHeadingContains('Envelope meta data')).to.equal(true);
 
-    await caseDetailsPage.addEnvelopeDataItems('NEW_APPLICATION', '123456', 'test_po-box-jurisdiction', 'test_envelope');
+    await caseDetailsPage.addEnvelopeDataItems('NEW_APPLICATION', '123456', 'test_po-box-jurisdiction');
     await caseDetailsPage.addDateItems('deliveryDate');
     await caseDetailsPage.addDateItems('openingDate');
 
