bb2032c82f9ef7c5301b129b44c8098d81deb6ea
gokul-sol <gokul.sridharan@HMCTS.NET>
2021-03-24 12:43:06 +0000

    Set proxy flag to false

--- e2e/service.conf.js
+++ e2e/service.conf.js
@@ -14,7 +14,7 @@ module.exports = {
 
   ProxyUrl: process.env.TEST_E2E_URL_PROXY || 'http://proxyout.reform.hmcts.net:8080',
   RunWithNumberOfBrowsers: process.env.TEST_E2E_NUM_BROWSERS || 2,
-  UseProxy: process.env.TEST_E2E_USE_PROXY !== 'false',
+  UseProxy: process.env.TEST_E2E_USE_PROXY === 'false',
   WaitForAngular: process.env.TEST_E2E_WAIT_FOR_ANGULAR !== 'false',
   FailFast: process.env.TEST_E2E_FAIL_FAST !== 'false',
 
