f07dfd5260ecb85cd119692d5df187dc84412b97
PhaniP <phani.perla@hmcts.net>
2021-04-08 11:12:12 +0100

    TA-610- Added missing paths

--- e2e/features/step_definitions/ucb-and-subscription.steps.ts
+++ e2e/features/step_definitions/ucb-and-subscription.steps.ts
@@ -73,9 +73,11 @@ Then(/^I enter "(.+)" to appointee and continue$/, async function (hasAppointee)
      await anyCcdPage.clickElementById('appeal_appellant_isAppointee-Yes');
      await appointeePage.addAppointeeDetails()
      browser.driver.sleep(10);
+     await anyCcdPage.click('Continue');
+     browser.driver.sleep(10);
      await anyCcdPage.click('Submit');
      browser.driver.sleep(10);
-     await anyCcdPage.click('History');
+     await anyCcdPage.clickTab('History');
 
      expect(await anyCcdPage.contentContains('Awaiting Admin Action')).to.equal(true);
      browser.driver.sleep(5);
